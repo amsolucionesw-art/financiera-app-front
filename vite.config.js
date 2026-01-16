6379509377c43c65d5cd0e2c160362112641e61d
syesistemaapp <syesistemaapp@gmail.com>
2026-01-15 22:20:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,22 @@ export default defineConfig({
   resolve: {
     dedupe: ['react', 'react-dom']
   },
+
   plugins: [
     react(),
     tailwindcss()
   ],
-})
 
+  /**
+   * Netlify estaba construyendo con targets tipo "es2020/chrome87"
+   * y eso NO soporta top-level await.
+   * Subimos el target para permitirlo.
+   */
+  build: {
+    target: 'es2022'
+  },
+
+  esbuild: {
+    target: 'es2022'
+  }
+})
